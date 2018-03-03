b7fd32a19a63e3814133e2eb4fc43c0b0aae9b00
infyhr <infy@cybershade.org>
2018-03-03 21:54:06 +0100

    Added RunePage DTO

---
+++ MMBuddy/Dtos/RunePage.cs
@@ -0,0 +1,12 @@
+﻿namespace MMBuddy.Dtos
+{
+    public class RunePage
+    {
+        public bool current { get; set; }
+        public bool isActive { get; set; }
+        public string name { get; set; }
+        public int primaryStyleId { get; set; }
+        public int subStyleId { get; set; }
+        public int[] selectedPerkIds { get; set; }
+    }
+}
