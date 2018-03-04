dea0a031fba9c83e99c59f192c493cf062c171e5
infyhr <infy@cybershade.org>
2018-03-04 13:44:15 +0100

    Add Champion and Conversation DTOs

---
+++ MMBuddy/Dtos/Champion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace MMBuddy.Dtos
+{
+    public class ChampionWrapperList
+    {
+        public string Type { get; set; }
+        public string Version { get; set; }
+        public Dictionary<string, Champion> Data { get; set; }
+    }
+
+    public class Champion
+    {
+        public string Title { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ MMBuddy/Dtos/Conversation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMBuddy.Dtos
+{
+    public class Conversation
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+
+        // There's more stuff, albeit useless to us
+    }
+}
