1f64107e8c92cd5f85cb2f11e7fd5df0c7eee89e
infyhr <infy@cybershade.org>
2018-03-04 16:19:52 +0100

    Add PatchAsync

--- MMBuddy/Services/ApiClient.cs
+++ MMBuddy/Services/ApiClient.cs
@@ -14,6 +14,89 @@ using System.Threading.Tasks;
 
 namespace MMBuddy.Services
 {
+    /// <summary>
+    /// https://stackoverflow.com/a/39450277
+    /// </summary>
+    public static class HttpClientExtensions
+    {
+        /// <summary>
+        /// Send a PATCH request to the specified Uri as an asynchronous operation.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// Returns <see cref="T:System.Threading.Tasks.Task`1"/>.The task object representing the asynchronous operation.
+        /// </returns>
+        /// <param name="client">The instantiated Http Client <see cref="HttpClient"/></param>
+        /// <param name="requestUri">The Uri the request is sent to.</param>
+        /// <param name="content">The HTTP request content sent to the server.</param>
+        /// <exception cref="T:System.ArgumentNullException">The <paramref name="client"/> was null.</exception>
+        /// <exception cref="T:System.ArgumentNullException">The <paramref name="requestUri"/> was null.</exception>
+        public static Task<HttpResponseMessage> PatchAsync(this HttpClient client, string requestUri, HttpContent content)
+        {
+            return client.PatchAsync(CreateUri(requestUri), content);
+        }
+
+        /// <summary>
+        /// Send a PATCH request to the specified Uri as an asynchronous operation.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// Returns <see cref="T:System.Threading.Tasks.Task`1"/>.The task object representing the asynchronous operation.
+        /// </returns>
+        /// <param name="client">The instantiated Http Client <see cref="HttpClient"/></param>
+        /// <param name="requestUri">The Uri the request is sent to.</param>
+        /// <param name="content">The HTTP request content sent to the server.</param>
+        /// <exception cref="T:System.ArgumentNullException">The <paramref name="client"/> was null.</exception>
+        /// <exception cref="T:System.ArgumentNullException">The <paramref name="requestUri"/> was null.</exception>
+        public static Task<HttpResponseMessage> PatchAsync(this HttpClient client, Uri requestUri, HttpContent content)
+        {
+            return client.PatchAsync(requestUri, content, CancellationToken.None);
+        }
+        /// <summary>
+        /// Send a PATCH request with a cancellation token as an asynchronous operation.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// Returns <see cref="T:System.Threading.Tasks.Task`1"/>.The task object representing the asynchronous operation.
+        /// </returns>
+        /// <param name="client">The instantiated Http Client <see cref="HttpClient"/></param>
+        /// <param name="requestUri">The Uri the request is sent to.</param>
+        /// <param name="content">The HTTP request content sent to the server.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <exception cref="T:System.ArgumentNullException">The <paramref name="client"/> was null.</exception>
+        /// <exception cref="T:System.ArgumentNullException">The <paramref name="requestUri"/> was null.</exception>
+        public static Task<HttpResponseMessage> PatchAsync(this HttpClient client, string requestUri, HttpContent content, CancellationToken cancellationToken)
+        {
+            return client.PatchAsync(CreateUri(requestUri), content, cancellationToken);
+        }
+
+        /// <summary>
+        /// Send a PATCH request with a cancellation token as an asynchronous operation.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// Returns <see cref="T:System.Threading.Tasks.Task`1"/>.The task object representing the asynchronous operation.
+        /// </returns>
+        /// <param name="client">The instantiated Http Client <see cref="HttpClient"/></param>
+        /// <param name="requestUri">The Uri the request is sent to.</param>
+        /// <param name="content">The HTTP request content sent to the server.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <exception cref="T:System.ArgumentNullException">The <paramref name="client"/> was null.</exception>
+        /// <exception cref="T:System.ArgumentNullException">The <paramref name="requestUri"/> was null.</exception>
+        public static Task<HttpResponseMessage> PatchAsync(this HttpClient client, Uri requestUri, HttpContent content, CancellationToken cancellationToken)
+        {
+            return client.SendAsync(new HttpRequestMessage(new HttpMethod("PATCH"), requestUri)
+            {
+                Content = content
+            }, cancellationToken);
+        }
+
+        private static Uri CreateUri(string uri)
+        {
+            return string.IsNullOrEmpty(uri) ? null : new Uri(uri, UriKind.RelativeOrAbsolute);
+        }
+    }
+
     public class LoggingHandler : DelegatingHandler
     {
         public LoggingHandler(HttpMessageHandler innerHandler)
@@ -49,6 +132,10 @@ namespace MMBuddy.Services
     {
         private static readonly HttpClient _httpClient = new HttpClient(new LoggingHandler(new HttpClientHandler()));
 
+        /// <summary>
+        /// Initializes the API client
+        /// </summary>
+        /// <param name="ServerInfo">The ServerInfo struct containing server information</param>
         public static void Initialize(ServerInfo ServerInfo)
         {
             // Set up the HTTP Client
@@ -108,6 +195,47 @@ namespace MMBuddy.Services
             return true;
         }
 
+        /// <summary>
+        /// Hovers a champion
+        /// </summary>
+        /// <param name="ChampionId">The champion's id to hover</param>
+        /// <param name="Completed">Complete the hover</param>
+        /// <returns></returns>
+        public static async Task<bool> Hover(int? PlayerId, int ChampionId, bool Completed)
+        {
+            var body = new
+            {
+                championId = ChampionId,
+                completed = Completed
+            };
+
+            HttpResponseMessage response = await _httpClient.PatchAsync(
+                $"/lol-champ-select/v1/session/actions/{PlayerId}",
+                body.AsJson());
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns the current matchmaking session if it exists
+        /// </summary>
+        /// <returns>The current matchmaking session, otherwise null</returns>
+        public static async Task<Session> GetSession()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("/lol-champ-select/v1/session");
+            if (response.IsSuccessStatusCode)
+            {
+                return JsonConvert.DeserializeObject<Session>(
+                    await response.Content.ReadAsStringAsync());
+            }
+
+            return null;
+        }
 
         #region Helpers
 
